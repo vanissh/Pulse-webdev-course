b2520a34d7e9591b6fd042af227e4f593a759d0f
Anna <sutcliff0684@gmail.com>
2022-06-19 18:43:16 +0300

    catalog section and double-sided cards

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+//slider
 const slider = tns({
     container: '.carousel__inner',
     items: 1,
@@ -23,4 +24,17 @@ document.querySelector('.prev').addEventListener('click', function () {
 document.querySelector('.next').addEventListener('click', function () {
   slider.goTo('next');
 });
+
+//catalog
+document.querySelector('.catalog').addEventListener('click', (e) => {
+  let target = e.target;
+
+  if(target.closest('.card__more')){
+    target.closest('.card').classList.add('card-active')
+  }
+
+  if(target.closest('.card__back')){
+    target.closest('.card').classList.remove('card-active')
+  }
+})
 
