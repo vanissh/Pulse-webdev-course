0d74655c8ccd48b19eba874233fa96a211085001
Anna <sutcliff0684@gmail.com>
2022-06-19 14:54:45 +0300

    slider without jquery

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,16 @@
-$(document).ready(function(){
-  $('.carousel__inner').slick({
-    infinite: true,
-    prevArrow: `<button type="button" class="slick-prev">
-                    <img src="icons/left_arrow.png"/>
-                </button>`, 
-    nextArrow: `<button type="button" class="slick-next">
-                    <img src="icons/right_arrow.png"/>
-                </button>`, 
-  });
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    nav: false,
+    controls: false
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
 });
